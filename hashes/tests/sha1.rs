0c4c7ad81f7e27dc831389dda5efc20de0f7ea56
karanabe <152078880+karanabe@users.noreply.github.com>
2024-01-01 02:10:06 +0900

    test(hash): add sha1 test for long message
    Test when a message is split with more than 512 bits of input.

--- hashes/tests/sha1.rs
+++ hashes/tests/sha1.rs
@@ -39,3 +39,15 @@ fn sha1_blank() {
 
     assert_eq!("da39a3ee5e6b4b0d3255bfef95601890afd80709", result);
 }
+
+#[test]
+fn sha1_3block_long() {
+    let message: &[u8] = "Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2Ac3Ac4Ac5Ac6Ac7Ac8Ac9Ad0Ad1Ad2Ad3Ad4Ad5Ad6Ad7Ad8Ad9Ae0Ae1Ae2Ae3Ae4Ae5Ae6Ae7Ae8Ae9Af0Af1Af2Af3Af4Af5Af6Af7Af8Af9Ag0Ag1Ag2Ag3".as_bytes();
+    let mut hashtest = sha1::Sha1::new();
+    hashtest.update(message);
+    let hashtest = hashtest.finalize();
+    let result = format!("{}", hashtest);
+    println!("{}", result);
+
+    assert_eq!("3400932512e8e8fc2b51c9d8784dffbe1495b449", result);
+}
